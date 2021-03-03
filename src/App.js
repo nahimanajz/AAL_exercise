a7b06f40436f9cb3d974b788280d57fe43721723
nahimanajz <jazzonahimana@gmail.com>
2021-03-03 22:33:15 +0000

    add exercise managing state

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const value1 = Math.floor(Math.random() * 100);
+const value2 = Math.floor(Math.random() * 100);
+const value3 = Math.floor(Math.random() * 100);
+const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
+const numQuestions = 0;
+const numCorrect = 0;
+
+class App extends Component {
+   state = {
+   		x: 1,
+ 		y: 4,
+		z: 6.0,
+     	p: 1.0 ,
+        numOfQeuestion: 0,
+        numCorrect:  0 
+   };
+   handleClick =() => {
+     
+     this.setState((prevState) => ({
+       x:  Math.floor(Math.random() * 100),
+       y: prevState.y + Math.floor(Math.random() * 100),
+       z: prevState.z + Math.floor(Math.random() * 100),
+       p:  prevState.x + prevState.y + prevState.z,
+       numOfQeuestion: prevState.numOfQeuestion +1,
+       numCorrect: proposedAnswer === prevState.x + prevState.y + prevState.z  ? prevState.numCorrect : prevState.numCorrect++
+     })
+    );
+
+        }
+  render() {
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="game">
+          <h2>Mental Math</h2>
+          <div className="equation">
+            <p className="text">{`${this.state.x} + ${this.state.y} + ${this.state.z} = ${this.state.p}`}</p>
+          </div>
+          <button onClick={this.handleClick}>True</button>
+          <button onClick={this.handleClick}>False</button>
+          <p className="text">
+            Your Score: {this.state.numCorrect}/{this.state.numOfQeuestion}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
