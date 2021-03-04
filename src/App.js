f499136681bf853bc82ae6c8bf7a5ce0a69e3f68
nahimanajz <jazzonahimana@gmail.com>
2021-03-04 11:32:52 +0000

    add controlled component

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+  text: ''
+  };
+
+  handleChange(writtenText) {
+   this.setState(()=>({text: writtenText}));
+  }
+  render() {
+   
+	const { text } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input type="text" placeholder="Say Something" value= {text}
+              onChange={(e)=> this.handleChange(e.target.value)} />
+          <p className="echo">Echo:</p>
+          <p>{text}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
